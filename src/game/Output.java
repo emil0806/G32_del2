2991ecf4ffe4c7109063b3a554534d967995b8ce
William Ajspur <wilajs23@gmail.com>
2023-10-10 16:49:44 +0200

    Adding displayRules

--- src/game/Output.java
+++ src/game/Output.java
@@ -3,6 +3,9 @@ package game;
 public class Output {
 
     public void displayRules() {
+        System.out.println("You need to get over 3000 in your balance to win the game");
+        System.out.println("If you get the sum 10, you lose 80 in your balance, but get an extra turn");
+        System.out.println("You start with a balance of 1000");
 
     }
 
