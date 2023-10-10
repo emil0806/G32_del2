7c2e2cb41aac53aefd1e7d44a24b43f1f7789e87
Lau <mikkel2011@hotmail.com>
2023-10-10 16:30:09 +0200

    fixing conflict

--- src/game/Field.java
+++ src/game/Field.java
@@ -2,4 +2,13 @@ package game;
 
 public class Field {
 
+    private String name;
+    private String description;
+    private int value;
+
+    public Field (String name, String description, int value){
+        this.name = name;
+        this.description = description;
+        this.value = value;
+    }
 }
