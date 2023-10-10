1302c191bd9a8758fd2cc4bfd459d192b7db022d
Emil Lauritzen <emil6008@gmail.com>
2023-10-10 17:51:34 +0200

    get methods in field class

--- src/game/Field.java
+++ src/game/Field.java
@@ -6,9 +6,21 @@ public class Field {
     private String description;
     private int value;
 
-    public Field (String name, String description, int value){
+    public Field(String name, String description, int value) {
         this.name = name;
         this.description = description;
         this.value = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getValue() {
+        return this.value;
     }
 }
