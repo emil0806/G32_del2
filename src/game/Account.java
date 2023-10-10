fe7f9ab099ee09aa69f03d23b5d9ae59030fcd2a
William Ajspur <wilajs23@gmail.com>
2023-10-10 15:52:38 +0200

    Changes in account

--- src/game/Account.java
+++ src/game/Account.java
@@ -4,4 +4,33 @@ public class Account {
 
     private int amount;
 
+    public Account() {
+    }
+
+    public void setAmount(int value) {
+        if (value <= 0) {
+            withdrawMoney(value);
+        } else {
+            depositMoney(value);
+        }
+
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public boolean withdrawMoney(int amount) {
+        if (this.amount - amount <= 0) {
+            this.amount = 0;
+        } else {
+            this.amount -= amount;
+        }
+        return true;
+    }
+
+    public boolean depositMoney(int amount) {
+        this.amount += amount;
+        return true;
+    }
 }
