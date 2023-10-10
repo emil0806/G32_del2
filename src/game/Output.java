666ec312652ca8559690125ef37866cd5b857566
William Ajspur <wilajs23@gmail.com>
2023-10-10 16:47:50 +0200

    Output methods

--- src/game/Output.java
+++ src/game/Output.java
@@ -2,4 +2,32 @@ package game;
 
 public class Output {
 
+    public void displayRules() {
+
+    }
+
+    public void displayPlayerName() {
+
+    }
+
+    public void showCoinflip() {
+
+    }
+
+    public void playerStarts() {
+
+    }
+
+    public void sumDice() {
+
+    }
+
+    public void winnerGame() {
+
+    }
+
+    public void displayScoreboard() {
+
+    }
+
 }
