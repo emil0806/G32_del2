6de781829fd37eb93dbe440e9a21d06a7686ff7d
Emil Lauritzen <emil6008@gmail.com>
2023-10-10 15:38:05 +0200

    Attributes in game class

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,14 @@
 package game;
 
+import java.util.Scanner;
+
 public class Game {
+    private Player player1 = new Player();
+    private Player player2 = new Player();
+    private Cup cup = new Cup();
+
+    private int turnCount = 0;
+
+    private Scanner scanner = new java.util.Scanner(System.in);
 
 }
