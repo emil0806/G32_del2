c539f1c84e0a87f0cd567166365543b38821ecd8
Emil Lauritzen <emil6008@gmail.com>
2023-10-10 21:41:25 +0200

    Setup for test

---
+++ src/test/Balance.java
@@ -0,0 +1,21 @@
+package test;
+
+import game.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Balance {
+    private Player player1 = new Player("Player 1");
+    private Cup cup = new Cup();
+    private Board board = new Board();
+
+    @Test
+    public void startBalance() {
+
+    }
+
+    @Test
+    public void negativeBalance() {
+
+    }
+}
