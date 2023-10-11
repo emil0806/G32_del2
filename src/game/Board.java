342ff0619508bc11981562307b4c65dfbf7ef39c
Emil Lauritzen <emil6008@gmail.com>
2023-10-11 16:49:43 +0200

    Additional description for every field

--- src/game/Board.java
+++ src/game/Board.java
@@ -5,18 +5,24 @@ public class Board {
     private Field[] fields = new Field[12];
 
     public Board() {
-        fields[0] = new Field("Tower", "You found Tower seven", 250);
-        fields[1] = new Field("Crater", "You fell into the Crater of Doom", -100);
-        fields[2] = new Field("Palace gates", "You found the gates to the migthy Palace", 100);
-        fields[3] = new Field("Cold desert", "uhhh, you are frezzing in the cold", -20);
-        fields[4] = new Field("Walled city", "You moved into the Walled city, you are safe", 180);
-        fields[5] = new Field("Monastery", "You fell a sleep in a church", 0);
-        fields[6] = new Field("Black cave", "You got bit by a bat, and starts felling sick", -70);
-        fields[7] = new Field("Huts in the mountain", "You found a shelter for the night", 60);
-        fields[8] = new Field("The Werewall", "There is a smell of werewolf, you run so fast, you get a extra turn",
+        fields[0] = new Field("Tower", "You found Tower seven. The government pays 250 for all your help.", 250);
+        fields[1] = new Field("Crater", "You fell into the Crater of Doom. You need to pay 100 to respawn.", -100);
+        fields[2] = new Field("Palace gates",
+                "You found the gates to the migthy Palace. They pay you 100 to keep it a secret.", 100);
+        fields[3] = new Field("Cold desert", "Uhhh, you are frezzing in the cold. You pay 20 bucks for a jacket.", -20);
+        fields[4] = new Field("Walled city",
+                "You moved into the Walled city, you are safe. Salary is good there, you earn 180.", 180);
+        fields[5] = new Field("Monastery", "You fell a sleep in a church. God let you stay for free.", 0);
+        fields[6] = new Field("Black cave",
+                "You got bit by a bat, and starts felling sick. The doctor needs 70 bucks to heal you.", -70);
+        fields[7] = new Field("Huts in the mountain",
+                "You found a shelter for the night, and somebody left 60 bucks in the shelter.", 60);
+        fields[8] = new Field("The Werewall",
+                "There is a smell of werewolf, you run so fast, you get a extra turn. But unfortunately you drop 80 bucks in the rush.",
                 -80);
-        fields[9] = new Field("The pit", "Ohhh no, not the pit again", -50);
-        fields[10] = new Field("Goldmine", "Ouch, you fell over a rock, a rock of gold!!!", 650);
+        fields[9] = new Field("The pit", "Ohhh no, not the pit again. Help cost 50 bucks.", -50);
+        fields[10] = new Field("Goldmine",
+                "Ouch, you fell over a rock, a rock of gold!!! The pawnshop pay 650 for the gold. ", 650);
     }
 
     public Field getField(int sum) {
